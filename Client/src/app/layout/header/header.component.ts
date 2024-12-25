8902925018238b83b5f58d89578457a2806aad95
asimrazzaq46 <asimrazzaq46@hotmail.com>
2024-12-25 16:40:54 +0100

    added the header in client side

---
+++ Client/src/app/layout/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatBadgeModule } from '@angular/material/badge';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+   
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
--- Server/Core/Params/ProductSpecsSearchParams.cs
+++ Server/Core/Params/ProductSpecsSearchParams.cs
@@ -40,7 +40,4 @@ public class ProductSpecsSearchParams
 		set => _serach = value.ToLower(); 
 	}
 
-
-
-
 }
